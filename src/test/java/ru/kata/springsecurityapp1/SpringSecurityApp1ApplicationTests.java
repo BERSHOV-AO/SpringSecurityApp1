fdcdff565005616ffae1286f64b79038403c26c9
Aleksey Bershov <bershov.aleksey@bk.ru>
2025-04-06 17:43:00 +0300

    Initial commit

---
+++ src/test/java/ru/kata/springsecurityapp1/SpringSecurityApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kata.springsecurityapp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApp1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
