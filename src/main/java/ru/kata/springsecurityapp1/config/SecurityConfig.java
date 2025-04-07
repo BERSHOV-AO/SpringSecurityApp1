8d1949c96ae41f1513117ec0fd0b2a03bb69d24c
Aleksey Bershov <bershov.aleksey@bk.ru>
2025-04-07 10:02:43 +0300

    Add SecurityConfig -> configure(HttpSecurity http)

--- src/main/java/ru/kata/springsecurityapp1/config/SecurityConfig.java
+++ src/main/java/ru/kata/springsecurityapp1/config/SecurityConfig.java
@@ -2,23 +2,67 @@ package ru.kata.springsecurityapp1.config;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import ru.kata.springsecurityapp1.security.AuthProviderImpl;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import ru.kata.springsecurityapp1.services.PersonDetailsService;
+//import ru.kata.springsecurityapp1.security.AuthProviderImpl;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final AuthProviderImpl authProvider;
+
+    private final PersonDetailsService personDetailsService;
 
     @Autowired
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+    public SecurityConfig(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
     }
 
-    // Настраивает аутентификацию
-    protected void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(authProvider);
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // конфигурируем сам Spring Security (какая страничка отвечает за вход, какая за ошибки)
+        // конфигурируем авторизацию
+
+        //настройка страницы логина
+        http.csrf().disable()                                                       // временно отключаем токен защиту от межсайтовой подделки запросов
+                .authorizeRequests()
+                .antMatchers("/auth/login", "/error").permitAll()               // пускаем любого пользователя на эти две станицы
+                .anyRequest().authenticated()                                       // на все остальные, только авторизованных
+                .and()                                                              // конкатенация настроек
+                .formLogin()
+                .loginPage("/auth/login")
+                .loginProcessingUrl("/process_login")                              // автоматически присылает для сравнения пароля
+                .defaultSuccessUrl("/hello", true)           // после успешной аутентификации перенаправляется на "/hello"
+                .failureUrl("/auth/login?error");                // не успешная аутентиф.
     }
+
+    // настройка аутентификации
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(personDetailsService);
+    }
+
+    // алгоритм расшифровки пароля
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance(); // не шифруем
+    }
+
+    //----------------------------------------------------------
+
+//    private final AuthProviderImpl authProvider;
+//
+//    @Autowired
+//    public SecurityConfig(AuthProviderImpl authProvider) {
+//        this.authProvider = authProvider;
+//    }
+
+    // Настраивает аутентификацию
+//    protected void configure(AuthenticationManagerBuilder auth) {
+//        auth.authenticationProvider(authProvider);
+//    }
 }
--- src/main/java/ru/kata/springsecurityapp1/security/AuthProviderImpl.java
+++ src/main/java/ru/kata/springsecurityapp1/security/AuthProviderImpl.java
@@ -1,47 +1,47 @@
-package ru.kata.springsecurityapp1.security;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Component;
-import ru.kata.springsecurityapp1.services.PersonDetailsService;
-
-import java.util.Collections;
-
-@Component
-public class AuthProviderImpl implements AuthenticationProvider {
-
-    private final PersonDetailsService personDetailsService;
-
-    @Autowired
-    public AuthProviderImpl(PersonDetailsService personDetailsService) {
-        this.personDetailsService = personDetailsService;
-    }
-
-    @Override
-    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        String username = authentication.getName();
-
-        UserDetails personDetails = personDetailsService.loadUserByUsername(username);
-
-        String password = authentication.getCredentials().toString();
-
-        System.out.println("password" + password);
-        System.out.println(password.equals(personDetails.getPassword()));
-
-        if (!password.equals(personDetails.getPassword()))
-            throw new BadCredentialsException("Incorrect password");
-
-        return new UsernamePasswordAuthenticationToken(personDetails, password,
-                Collections.emptyList());
-    }
-
-    @Override
-    public boolean supports(Class<?> aClass) {
-        return true;
-    }
-}
+//package ru.kata.springsecurityapp1.security;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.authentication.AuthenticationProvider;
+//import org.springframework.security.authentication.BadCredentialsException;
+//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.AuthenticationException;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.stereotype.Component;
+//import ru.kata.springsecurityapp1.services.PersonDetailsService;
+//
+//import java.util.Collections;
+//
+//@Component
+//public class AuthProviderImpl implements AuthenticationProvider {
+//
+//    private final PersonDetailsService personDetailsService;
+//
+//    @Autowired
+//    public AuthProviderImpl(PersonDetailsService personDetailsService) {
+//        this.personDetailsService = personDetailsService;
+//    }
+//
+//    @Override
+//    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+//        String username = authentication.getName();
+//
+//        UserDetails personDetails = personDetailsService.loadUserByUsername(username);
+//
+//        String password = authentication.getCredentials().toString();
+//
+//        System.out.println("password" + password);
+//        System.out.println(password.equals(personDetails.getPassword()));
+//
+//        if (!password.equals(personDetails.getPassword()))
+//            throw new BadCredentialsException("Incorrect password");
+//
+//        return new UsernamePasswordAuthenticationToken(personDetails, password,
+//                Collections.emptyList());
+//    }
+//
+//    @Override
+//    public boolean supports(Class<?> aClass) {
+//        return true;
+//    }
+//}
